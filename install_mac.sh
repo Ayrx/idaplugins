fde0cbf9dd333b9ab385bb8dcb0c8020365e2e3e
Terry Chia <terrycwk1994@gmail.com>
2018-10-13 21:33:28 +0800

    Use ln -snf for links to directories.

--- install_mac.sh
+++ install_mac.sh
@@ -4,11 +4,11 @@
 ln -sf ${PWD}/lighthouse/plugin/lighthouse_plugin.py \
 	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse_plugin.py
 
-ln -sf ${PWD}/lighthouse/plugin/lighthouse \
+ln -snf ${PWD}/lighthouse/plugin/lighthouse \
 	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse
 
 ln -sf ${PWD}/IDASkins/plugins/idaskins.py \
 	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/idaskins.py
 
-ln -sf ${PWD}/IDASkins/plugins/idaskins \
+ln -snf ${PWD}/IDASkins/plugins/idaskins \
 	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/idaskins
