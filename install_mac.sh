e7bb3018e9f0ca8c230746ccac1ab74653094d70
Terry Chia <terrycwk1994@gmail.com>
2018-10-13 21:04:48 +0800

    Initial commit.

---
+++ install_mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+ln -s ${PWD}/lighthouse/plugin/lighthouse_plugin.py \
+	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse_plugin.py
+
+ln -s ${PWD}/lighthouse/plugin/lighthouse \
+	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse
