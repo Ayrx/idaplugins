0ce55804589a15f6a7c3bf4de610bd6668f28b2a
Terry Chia <terrycwk1994@gmail.com>
2018-10-13 21:24:15 +0800

    Add IDASkins plugin.

--- install_mac.sh
+++ install_mac.sh
@@ -6,3 +6,9 @@ ln -sf ${PWD}/lighthouse/plugin/lighthouse_plugin.py \
 
 ln -sf ${PWD}/lighthouse/plugin/lighthouse \
 	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse
+
+ln -sf ${PWD}/IDASkins/plugins/idaskins.py \
+	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/idaskins.py
+
+ln -sf ${PWD}/IDASkins/plugins/idaskins \
+	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/idaskins
