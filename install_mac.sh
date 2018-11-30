7178912894fae2368d5ba822c99572a8fcda9c51
Terry Chia <terrycwk1994@gmail.com>
2018-11-30 22:43:28 +0800

    Change install directory for plugins.

--- install_mac.sh
+++ install_mac.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+PLUGINDIR=${HOME}/.idapro/plugins
 
 ln -sf ${PWD}/lighthouse/plugin/lighthouse_plugin.py \
-	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse_plugin.py
+	${PLUGINDIR}/lighthouse_plugin.py
 
 ln -snf ${PWD}/lighthouse/plugin/lighthouse \
-	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/lighthouse
+	${PLUGINDIR}/lighthouse
 
 ln -sf ${PWD}/IDASkins/plugins/idaskins.py \
-	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/idaskins.py
+	${PLUGINDIR}/idaskins.py
 
 ln -snf ${PWD}/IDASkins/plugins/idaskins \
-	/Applications/IDA\ Pro\ 7.0/ida.app/Contents/MacOS/plugins/idaskins
+	${PLUGINDIR}/idaskins
